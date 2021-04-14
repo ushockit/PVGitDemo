8e7dd3be666d17d703b2df8e30abc225b6f721bb
Vitaliy Lyubimov <ushock.it@gmail.com>
2021-04-14 19:05:13 +0300

    Add action Start

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace SomeProject {
             switch(action) {
                 case 1:
                     Console.WriteLine("Starting...");
+                    Start();
                 break;
                 case 2:
                     Environment.Exit(0);
@@ -23,6 +24,19 @@ namespace SomeProject {
 
 
             Console.Read();
+        }
+
+
+        static void Start() {
+            while(true) {
+                Console.Clear();
+
+                for(int i = 0; i < 10; i++) {
+                    for(int j = 0; j < 10; j++) {
+
+                    }
+                }
+            }
         }
     }
 }
